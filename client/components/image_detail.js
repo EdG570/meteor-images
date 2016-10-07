843706aa93fa474274d02bbe11d1aca63efb9f47
EdG570 <cole570@hotmail.com>
2016-10-07 17:25:45 -0400

    Progress bar component added for upvotes and downvotes

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import ImageScore from './progress_bar';
+
 const ImageDetail = (props) => {
   return (
     <li className="media list-group-item">
       <div className="media-left">
-        <img src={props.source} alt={props.title}/>
+        <img src={props.image.link} alt={props.image.title}/>
       </div>
       <div className="media-body">
-        <h4 className="media-heading">{props.title}</h4>
+        <h4 className="media-heading">{props.image.title}</h4>
+        <p>{props.image.decription}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs}/>
       </div>
     </li>
   );
--- client/components/image_list.js
+++ client/components/image_list.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 import ImageDetail from './image_detail';
 
-const IMAGES = [
-  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
-  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
-  { title: 'Mug', link: 'http://dummyimage.com/600x400' }
-];
-
 const ImageList = (props) => {
-  const RenderedImages = props.images.map(image => {
-    return <ImageDetail key={image.title} 
-                        source={image.link} 
-                        title={image.title} 
-           />;
-  });
+  const validImages = props.images.filter(image => !image.is_album);
+
+  const RenderedImages = validImages.map(image => {
+    return (
+      <ImageDetail key={image.title} image={image}></ImageDetail>
+    );         
+  }); 
 
   return (
     <ul className="media-list list-group">
