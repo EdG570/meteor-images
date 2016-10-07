2bf6bfa507279eec7500f5604dfc050cc2375b07
EdG570 <cole570@hotmail.com>
2016-10-07 13:17:08 -0400

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,19 @@
+// Any JS in here is auto run for us
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      React App #2
+    </div>
+  );
+};
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.container'));
+});
+
+
+
