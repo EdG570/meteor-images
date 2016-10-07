58381e05e73c4f33bf958960b5f4f7219b5d957e
EdG570 <cole570@hotmail.com>
2016-10-07 16:19:52 -0400

    Added api response to state in App component

--- client/main.js
+++ client/main.js
@@ -1,17 +1,26 @@
-// Any JS in here is auto run for us
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageList: []
+    };
+  }
+
   componentWillMount() {
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-    .then(response => console.log(response));
+      .then(response => 
+        this.setState({ imageList: response.data.data })
+      );
   }
 
   render() {
+    console.log(this.state.imageList);
     return (
       <div>
         <ImageList />
